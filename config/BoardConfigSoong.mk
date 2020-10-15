5410f169f9cf05e7f7d458b6158e32bc239e941e
Pig <pig.priv@gmail.com>
2020-10-15 08:58:40 +0800

    [lineage-18.0] cryptfshw: Convert aospa product variables to soong config variables
    Change-Id: Ie02b9282b6595a9768cd5ec34febc3b5dbc4032e

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -24,6 +24,17 @@ SOONG_CONFIG_aospaGlobalVars += \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib
 
+SOONG_CONFIG_NAMESPACES += aospaQcomVars
+SOONG_CONFIG_aospaQcomVars += \
+    should_wait_for_qsee \
+    supports_hw_fde \
+    supports_hw_fde_perf 
+
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_aospaQcomVars += \
+    qcom_soong_namespace
+endif
+
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
@@ -35,6 +46,13 @@ SOONG_CONFIG_aospaGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_aospaGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_aospaGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_aospaGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_aospaQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
+SOONG_CONFIG_aospaQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
+SOONG_CONFIG_aospaQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
+
+ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+SOONG_CONFIG_aospaQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
+endif
 
 # Qualcomm variables
 SOONG_CONFIG_NAMESPACES += aosp_vs_qva
